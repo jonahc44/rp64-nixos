eb0e6bcd9f2664513eb99290a2413c84c577b7fa
jonahc44 <jonahc0508@gmail.com>
2026-07-04 16:11:59 -0500

    changing hardware config for cleaner directory layout

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,37 +10,17 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  # Set up to use ephemeral root
+  # Ephemeral root on tmpfs
   fileSystems."/" =
     { device = "none";
       fsType = "tmpfs";
       options = [ "defaults" "size=1G" "mode=755" ];
     };
-
-  fileSystems."/old-root" = 
+    
+  # Persistent Nix data (store, var, persist live on this partition)
+  fileSystems."/nix" =
     { device = "/dev/disk/by-uuid/0f99ab30-6e6f-460f-b416-826bed7c42cd";
       fsType = "ext4";
-      neededForBoot = true;
-    };
-
-  fileSystems."/nix/store" = 
-    { device = "/old-root/nix/store";
-      fsType = "none";
-      options = [ "bind" ];
-      neededForBoot = true;
-    };
-
-  fileSystems."/nix/var" = 
-    { device = "/old-root/nix/var";
-      fsType = "none";
-      options = [ "bind" ];
-      neededForBoot = true;
-    };
-
-  fileSystems."/nix/persist" = 
-    { device = "/old-root/nix/persist";
-      fsType = "none";
-      options = [ "bind" ];
       neededForBoot = true;
     };
 
