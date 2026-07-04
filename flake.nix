c95c176af93147a5c1ecf23e7849fd1fa3088d45
jonahc44 <jonahc0508@gmail.com>
2026-07-04 13:37:41 -0500

    modifying flake for impermanence

--- flake.nix
+++ flake.nix
@@ -3,14 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
     
     # The repository for hardware-specific configurations
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }: {
+  outputs = { self, nixpkgs, nixos-hardware, impermanence, ... }: {
     nixosConfigurations.rockpro64 = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
+
+      specialArgs = { inherit impermanence; };
       
       modules = [
         # Pull in the RockPro64 specific hardware quirks
