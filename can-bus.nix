ed97d9a211e5970fe7a6631fe1418542c3e756dd
jonahc44 <jonahc0508@gmail.com>
2026-07-04 21:07:42 -0500

    starting to set up can bus with device tree

---
+++ can-bus.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  boot.kernelModules = [ "spi-rockchip" "mcp251x" "can" "can_raw" "can_dev" ];
+
+  hardware.deviceTree = {
+    enable = true;
+    name = "rockchip/rk3399-rockpro64.dtb";
+    overlays = [
+      {
+        name = "mcp2515-can0";
+        dtsFile = ./mcp2515-overlay.dts; 
+      }
+    ];
+  };
+
+  systemd.services.can0-up = {
+    description = "Initialize can0 SocketCAN interface";
+    bindsTo = [ "sys-subsystem-net-devices-can0.device" ];
+    after = [ "sys-subsystem-net-devices-can0.device" ];
+    wantedBy = [ "multi-user.target" ];
+    
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.iproute2}/bin/ip link set can0 up type can bitrate 500000";
+      ExecStop = "${pkgs.iproute2}/bin/ip link set can0 down";
+    };
+  };
+}
